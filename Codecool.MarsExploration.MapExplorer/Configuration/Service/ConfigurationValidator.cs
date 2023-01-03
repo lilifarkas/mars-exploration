061d9b4bce3fc56377b8ff30f65add1c58a14052
lili.farkas <lili.farkas14@gmail.com>
2023-01-03 20:48:30 +0100

    Coordinate validator

--- Codecool.MarsExploration.MapExplorer/Configuration/Service/ConfigurationValidator.cs
+++ Codecool.MarsExploration.MapExplorer/Configuration/Service/ConfigurationValidator.cs
@@ -8,15 +8,31 @@ public class ConfigurationValidator : IConfigurationValidator
     public bool Validate(SimulationParameters simulationParameters, MapLoader.MapLoader mapLoader)
     {
         var map = mapLoader.Load(simulationParameters.MapFile);
-        if (simulationParameters.StepsToTimeOut > 0 && simulationParameters.SymbolsOfTheResources != null)
+        if (CoordinateValidator(map.Representation, simulationParameters.Coordinate) 
+            && simulationParameters.StepsToTimeOut > 0
+            && simulationParameters.SymbolsOfTheResources != null)
         {
             return true;
         }
-        throw new NotImplementedException();
+
+        return false;
     }
 
-    private static bool CoordinateValidator(Map map, Coordinate coordinate)
+    private static bool CoordinateValidator(string?[,] map, Coordinate coordinate)
     {
-        throw new NotImplementedException();
+        var allXCoordinates =
+            Enumerable.Range(coordinate.X, 1)
+                .Select(e => coordinate with { X = e });
+
+        var allYCoordinates =
+            Enumerable.Range(coordinate.Y, 1)
+                .Select(e => coordinate with { Y = e });
+        
+        return coordinate.X >= 0
+               && coordinate.X < map.GetLength(0)
+               && coordinate.Y >= 0
+               && coordinate.Y < map.GetLength(1)
+               && map[coordinate.X, coordinate.Y] == null
+               && allXCoordinates.Concat(allYCoordinates).All(c => map[c.X, c.Y] == null);
     }
 }
