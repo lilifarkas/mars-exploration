ce196c6a7d9b680f7e140691d6c1f58b6127de54
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-01-04 11:33:44 +0100

    rover record with properties

---
+++ Codecool.MarsExploration.MapExplorer/MarsRover/Model/Rover.cs
@@ -0,0 +1,5 @@
+﻿using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.MarsRover;
+
+public record Rover(string Id, Coordinate CurrentPosition, IEnumerable<Coordinate>VisibleTiles, IEnumerable<Coordinate>EncounteredResources);
