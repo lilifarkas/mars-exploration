5cea2afd3bfa9117fc34b2c9b42845f985857ef2
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-01-05 19:54:40 +0100

    create directional movement interface

---
+++ Codecool.MarsExploration.MapExplorer/Movement/IDirectionalMovement.cs
@@ -0,0 +1,9 @@
+﻿using Codecool.MarsExploration.MapExplorer.Direction;
+using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.Movement;
+
+public interface IDirectionalMovement
+{
+    public void Move();
+}
