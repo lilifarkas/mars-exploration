421aeac1e44e843bdac59b59fece6238ebdd4faa
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-01-05 17:29:30 +0100

    deletes config creator

--- Codecool.MarsExploration.MapExplorer/ConfigurationCreator/Service/IConfigCreator.cs
+++
@@ -1,9 +0,0 @@
-﻿using Codecool.MarsExploration.MapGenerator.Calculators.Model;
-
-namespace Codecool.MarsExploration.MapExplorer.ConfigurationCreator.Service;
-
-public interface IConfigCreator
-{
-    public Configuration.Configuration ConfigurationCreator(string mapFile, List<string> resources,
-        Coordinate landingCoordinate, int stepsToTimeout);
-}
