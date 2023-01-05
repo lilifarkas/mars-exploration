ed71605fb92aee40080fcffe1dd55a8df7990400
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-01-05 12:20:26 +0100

    add config creator

---
+++ Codecool.MarsExploration.MapExplorer/ConfigurationCreator/Service/IConfigCreator.cs
@@ -0,0 +1,9 @@
+﻿using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.ConfigurationCreator.Service;
+
+public interface IConfigCreator
+{
+    public Configuration.Configuration ConfigurationCreator(string mapFile, List<string> resources,
+        Coordinate landingCoordinate, int stepsToTimeout);
+}
