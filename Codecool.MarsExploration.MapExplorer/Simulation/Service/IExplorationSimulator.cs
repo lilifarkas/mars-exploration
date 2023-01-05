5be920c58c900f186f45d9e941723984ca51dc57
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-01-05 13:47:32 +0100

    interface for exploration simulator

---
+++ Codecool.MarsExploration.MapExplorer/Simulation/Service/IExplorationSimulator.cs
@@ -0,0 +1,10 @@
+﻿using Codecool.MarsExploration.MapExplorer.Simulation.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.Simulation.Service;
+
+public interface IExplorationSimulator
+{
+    SimulationContext GenerateSimulationContext();
+    void RunSimulation(Configuration.Configuration configuration);
+    void HandleOutcome();
+}
