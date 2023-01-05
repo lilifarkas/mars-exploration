23bde97f861669a30fdfbddfe746ed49c51678b0
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-01-05 17:57:10 +0100

    refractors constructor of ExplorationSimulator.cs

--- Codecool.MarsExploration.MapExplorer/Simulation/Service/ExplorationSimulator.cs
+++ Codecool.MarsExploration.MapExplorer/Simulation/Service/ExplorationSimulator.cs
@@ -3,6 +3,7 @@ using Codecool.MarsExploration.MapExplorer.Configuration.Service;
 using Codecool.MarsExploration.MapExplorer.Exploration;
 using Codecool.MarsExploration.MapExplorer.MapLoader;
 using Codecool.MarsExploration.MapExplorer.MarsRover;
+using Codecool.MarsExploration.MapExplorer.MarsRover.Service;
 using Codecool.MarsExploration.MapExplorer.Movement;
 using Codecool.MarsExploration.MapExplorer.Simulation.Model;
 using Codecool.MarsExploration.MapGenerator.Calculators.Model;
@@ -12,35 +13,34 @@ namespace Codecool.MarsExploration.MapExplorer.Simulation.Service;
 
 public class ExplorationSimulator : IExplorationSimulator
 {
-    private readonly Rover _rover;
     private readonly IMapLoader _mapLoader;
     private  readonly Random Random = new Random();
     private  IConfigurationValidator _configurationValidator;
     private  IOutcomeAnalyzer _lackOfResourcesAnalyzer;
     private  IOutcomeAnalyzer _succesAnalyzer;
     private  IOutcomeAnalyzer _timeOutanalyzer;
-    private  ExploringRoutine _exploringRoutine;
-    private  ReturningRoutine _returningRoutine;
+    private IRoverDeployer _roverDeployer;
 
-    public ExplorationSimulator(Rover rover, IMapLoader mapLoader, IConfigurationValidator configurationValidator,ExploringRoutine exploringRoutine, ReturningRoutine returningRoutine, IOutcomeAnalyzer lackOfResourcesAnalyzer, IOutcomeAnalyzer succesAnalyzer, IOutcomeAnalyzer timeOutanalyzer)
+    public ExplorationSimulator(IMapLoader mapLoader, IConfigurationValidator configurationValidator, IOutcomeAnalyzer lackOfResourcesAnalyzer, IOutcomeAnalyzer succesAnalyzer, IOutcomeAnalyzer timeOutanalyzer, IRoverDeployer roverDeployer)
     {
-        _rover = rover;
         _mapLoader = mapLoader;
         _configurationValidator = configurationValidator;
-        _exploringRoutine = exploringRoutine;
-        _returningRoutine = returningRoutine;
         _lackOfResourcesAnalyzer = lackOfResourcesAnalyzer;
         _succesAnalyzer = succesAnalyzer;
         _timeOutanalyzer = timeOutanalyzer;
+        _roverDeployer = roverDeployer;
     }
 
     public void RunSimulation(Configuration.Configuration configuration)
     {
         var map = _mapLoader.Load(configuration.MapFile);
         var landingSpot = CheckLandingSpotForClear(configuration.LandingSpot, map);
-        var simulationContext = new SimulationContext(0, configuration.StepsToTimeOut, _rover,
+        var rover = _roverDeployer.Deploy();
+        var simulationContext = new SimulationContext(0, configuration.StepsToTimeOut, rover,
             landingSpot, map, configuration.SymbolsOfTheResources);
+        ExploringRoutine exploringRoutine = new ExploringRoutine(simulationContext);
         
+        var finishedSimulationContext = SimulationLoop(simulationContext, exploringRoutine);
     }
 
     public SimulationContext HandleOutcome(SimulationContext simulationContext, ExplorationOutcome outcome)
@@ -48,14 +48,14 @@ public class ExplorationSimulator : IExplorationSimulator
         return simulationContext with { ExplorationOutcome = outcome };
     }
 
-    private SimulationContext SimulationLoop(SimulationContext simulationContext)
+    private SimulationContext SimulationLoop(SimulationContext simulationContext, ExploringRoutine exploringRoutine)
     {
         int step = 1;
         while (simulationContext.ExplorationOutcome == ExplorationOutcome.InProgress)
         {
             var message = $"STEP: {step}, POSITION: {simulationContext.Rover.CurrentPosition}";
             Console.WriteLine(message);
-            _exploringRoutine.Step(simulationContext.Rover);
+            exploringRoutine.Step(simulationContext.Rover);
             var results = new[] {
                 _lackOfResourcesAnalyzer.Analyze(simulationContext), _succesAnalyzer.Analyze(simulationContext),
                 _timeOutanalyzer.Analyze(simulationContext)
