fa3fc928bbcb3a7f8ec7abff672e7c1e394a8236
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-01-05 11:53:54 +0100

    refractor configuration validator to separately validate config options

--- Codecool.MarsExploration.MapExplorer/Configuration/Service/ConfigurationValidator.cs
+++ Codecool.MarsExploration.MapExplorer/Configuration/Service/ConfigurationValidator.cs
@@ -1,38 +1,31 @@
 using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+using Codecool.MarsExploration.MapGenerator.Calculators.Service;
 using Codecool.MarsExploration.MapGenerator.MapElements.Model;
 
 namespace Codecool.MarsExploration.MapExplorer.Configuration.Service;
 
 public class ConfigurationValidator : IConfigurationValidator
 {
-    public bool Validate(Configuration configuration, MapLoader.MapLoader mapLoader)
+    private readonly ICoordinateCalculator _coordinateCalculator = new CoordinateCalculator();
+    public bool LandingSpotValidate(Map map, Coordinate landingCoordinates)
     {
-        var map = mapLoader.Load(configuration.MapFile);
-        if (CoordinateValidator(map.Representation, configuration.LandingSpot) 
-            && configuration.StepsToTimeOut > 0
-            && configuration.SymbolsOfTheResources != null)
-        {
-            return true;
-        }
-
-        return false;
+        return map.Representation[landingCoordinates.X, landingCoordinates.Y] == " " &&
+               _coordinateCalculator.GetAdjacentCoordinates(landingCoordinates, 3).Any(coord => map.Representation[coord.X,coord.Y] == " ");
     }
 
-    private static bool CoordinateValidator(string?[,] map, Coordinate coordinate)
+    public bool MapFileValidate(string mapFile)
     {
-        var allXCoordinates =
-            Enumerable.Range(coordinate.X, 1)
-                .Select(e => coordinate with { X = e });
+        return mapFile.Length > 1;
+    }
 
-        var allYCoordinates =
-            Enumerable.Range(coordinate.Y, 1)
-                .Select(e => coordinate with { Y = e });
-        
-        return coordinate.X >= 0
-               && coordinate.X < map.GetLength(0)
-               && coordinate.Y >= 0
-               && coordinate.Y < map.GetLength(1)
-               && map[coordinate.X, coordinate.Y] == null
-               && allXCoordinates.Concat(allYCoordinates).All(c => map[c.X, c.Y] == null);
+    public bool ResourcesValidate(IEnumerable<string> symbolsOfTheResources)
+    {
+        var symbols = new List<string> { "#", "%", "*", "&" };
+        return symbols.Any(symbol=>symbolsOfTheResources.Any(resourceSymbol => resourceSymbol == symbol));
+    }
+
+    public bool StepsToTimeoutValidate(int stepsToTimeout)
+    {
+        return stepsToTimeout > 0;
     }
 }
--- Codecool.MarsExploration.MapExplorer/Configuration/Service/IConfigurationValidator.cs
+++ Codecool.MarsExploration.MapExplorer/Configuration/Service/IConfigurationValidator.cs
@@ -1,6 +1,12 @@
+using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+using Codecool.MarsExploration.MapGenerator.MapElements.Model;
+
 namespace Codecool.MarsExploration.MapExplorer.Configuration.Service;
 
 public interface IConfigurationValidator
 {
-    bool Validate(Configuration configuration, MapLoader.MapLoader mapLoader);
+    bool LandingSpotValidate(Map map, Coordinate landingCoordinates);
+    bool MapFileValidate(string mapFile);
+    bool ResourcesValidate(IEnumerable<string> symbolsOfTheResources);
+    bool StepsToTimeoutValidate(int stepsToTimeout);
 }
